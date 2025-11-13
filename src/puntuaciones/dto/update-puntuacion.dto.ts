7b02616498329f2bbeb73d8e4799892d7863020d
BastiHdez <148597391+BastiHdez@users.noreply.github.com>
2025-11-13 03:53:12 -0300

    Limpieza

--- src/puntuaciones/dto/update-puntuacion.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePuntuacionDto } from './create-puntuacion.dto';
-
-export class UpdatePuntuacionDto extends PartialType(CreatePuntuacionDto) {}
--- src/reseñas/dto/update-resena.dto.ts
+++
@@ -1,5 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateResenaDto } from './create-resena.dto';
-
-/** Permite actualizar calificacion/comentario (id_* no suele cambiar) */
-export class UpdateResenaDto extends PartialType(CreateResenaDto) {}
